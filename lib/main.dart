9adcaf3361d129728a411c2220310e01e5cf98e9
Sarocy <sarahocy@hotmail.com>
2024-08-27 13:14:51 -0300

    Projeto dia 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MeuApp());
+  runApp(const QuizApp());
 }
 
-class MeuApp extends StatelessWidget {
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Meu Primeiro App'),
+      title: 'Quiz AluraPop',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF357787),
+          primary: const Color(0xFF1f7788),
         ),
-        body: Text('Hello world :)'),
       ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color(0xFF01080E), Color.fromARGB(255, 5, 46, 63)],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GradientBackground(
+        child: Center(
+          child: const Text('Bem-vindo ao Quiz AluraPop!',
+            style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  textAlign: TextAlign.center,),
+        ),
+      )
     );
   }
 }
